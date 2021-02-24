96f64b6960d9ee61f8099ed5bde3d9094c8bbb1e
John Aron <johnaronbelmonte@gmail.com>
2021-02-24 23:55:55 +0800

    trying tkinter

--- conversion.py
+++ conversion.py
@@ -3,9 +3,9 @@ class Conversion:
 
     def csgo_to_valorant(self, csgo_sensitivity):
         final_sensitivity = csgo_sensitivity/3.18
-        print("Your valorant sensitivity should be: ", final_sensitivity)
+        return final_sensitivity
 
     def valorant_to_csgo(self, valorant_sensitivity):
         final_sensitivity = valorant_sensitivity * 3.18
-        print("Your csgo sensitivity should be: ", final_sensitivity)
+        return final_sensitivity
         
--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+from tkinter import *
+
 from conversion import Conversion
 
+root = Tk()
 csgo_sensitivity = 10.00
 valorant_sensitivity = 10.00
 
 convert = Conversion()
 
-convert.csgo_to_valorant(csgo_sensitivity)
-convert.valorant_to_csgo(valorant_sensitivity)
+csgo_result = convert.csgo_to_valorant(csgo_sensitivity)
+valorant_result = convert.valorant_to_csgo(valorant_sensitivity)
+
+def do_convert():
+    label = Label(root, text=csgo_result)
+    label.pack()
+
+button_convert = Button(root, text="convert", padx=20, command=do_convert)
+button_convert.pack()
+root.mainloop()
